d486e554122823d828f04bf3503e381b966ba775
Ranidhumma <ranidhumma8@gmail.com>
2022-02-23 10:05:10 +0530

    calculate wage

---
+++ src/com/java/EmployeeProblemOOPS.java
@@ -0,0 +1,37 @@
+package com.java;                        
+
+import java.util.Random;
+
+class Employee {
+    static final int IS_FULL_TIME = 1;
+    static final int EMP_WAGE_PER_DAY = 20;
+
+    public static int empCheck() {
+	Random random = new Random();
+	int empCheck = random.nextInt(2);
+	return empCheck;
+    }
+
+    public static void empSalary(int empCheck) {
+	int empWrkHrs = 0;
+	if (empCheck == IS_FULL_TIME) 
+	{
+	    empWrkHrs = 8;
+	}
+	else 
+	{
+	    empWrkHrs = 0;
+	}
+	int empWage = empWrkHrs * EMP_WAGE_PER_DAY;
+	System.out.println("empWage = " + empWage);
+    }
+}
+
+public class EmployeeProblemOOPS {
+
+    public static void main(String[] args) {
+	System.out.println("Here we are Calculating wage of employee per day");
+	int empCheck = Employee.empCheck();
+	Employee.empSalary(empCheck);
+    }
+}
